d6b6fc53a311da8d14a7719968e82aae45233bf3
Gabriel Amorim <gaahamorim95@gmail.com>
2021-10-23 00:36:13 -0300

    Class 1: github, auth, userInfo, prisma, msgs, last3, userProfile

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,28 @@
 import "dotenv/config";
 import express from "express";
+import http from "http";
+import { Server } from "socket.io"
+import { router } from "./routes";
+import cors from "cors";
 
 const app = express();
+app.use(cors())
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", socket => {
+  console.log(`Usuário conectado no socket ${socket.id}`)
+})
+
+app.use(express.json())
+
+app.use(router);
 
 app.get("/github", (req, res) => {
   res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
@@ -13,4 +34,4 @@ app.get("/signin/callback", (req, res) => {
   return res.json(code)
 })
 
-app.listen(4000, () => console.log(`Server is running on PORT 4000`))
+export { serverHttp, io };
